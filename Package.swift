9ac0161b610a07fb68607d16d63a8ce3aea3fb38
Tierry Hörmann <tierryh@student.ethz.ch>
2017-12-31 01:01:06 +0100

    removed unnecessary flag. todo: resolve strange issue, where the compiled ll file will interpret with lli, but after linkage gives a segfault

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .package(url: "https://github.com/trill-lang/LLVMSwift", .branch("master")),
         .package(url: "https://github.com/kylef/PathKit", .branch("master")),
         .package(url: "https://github.com/oarrabi/FileUtils", .branch("master")),
-        .package(url: "../GlibcVersion", .branch("master"))
+        .package(url: "https://github.com/tierriminator/GlibcVersion", .branch("master"))
     ],
     targets: [
         .target(name: "Lib", dependencies: ["LLVM", "PathKit", "FileUtils"]),
